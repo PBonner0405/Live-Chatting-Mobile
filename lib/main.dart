a53b19fd0283007902cb6d72ac29a0d1c13faeee
topdev10 <k100cool@oultook.com>
2019-12-24 17:00:03 +0800

    add icon color theme

--- lib/main.dart
+++ lib/main.dart
@@ -40,23 +40,26 @@ class MainScreenState extends State<MainScreen> {
   }
 
   Widget _buildTextComposer() {
-    return new Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: new Row(
-        children: <Widget>[
-          new Flexible(
-            child: new TextField(
-              controller: _textController,
-              onSubmitted: _handleSubmitted,
-              decoration: new InputDecoration.collapsed(hintText: "Send a Message"),
+    return new IconTheme(
+        data: new IconThemeData(color: Theme.of(context).accentColor),
+        child: new Container(
+            margin: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: new Row(
+              children: <Widget>[
+                new Flexible(
+                  child: new TextField(
+                    controller: _textController,
+                    onSubmitted: _handleSubmitted,
+                    decoration: new InputDecoration.collapsed(hintText: "Send a Message"),
+                  ),
+                ),
+                new Container(
+                  margin: new EdgeInsets.symmetric(horizontal: 4.0),
+                  child: new IconButton(icon: new Icon(Icons.send), onPressed: () => _handleSubmitted(_textController.text)),
+                )
+              ],
             ),
-          ),
-          new Container(
-            margin: new EdgeInsets.symmetric(horizontal: 4.0),
-            child: new IconButton(icon: new Icon(Icons.send), onPressed: () => _handleSubmitted(_textController.text)),
-          )
-        ],
-      )
+        ),
     );
   }
 }
