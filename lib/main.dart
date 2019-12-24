9c74d7ade5b009b5c29b6e27d24441e124ed24b6
topdev10 <k100cool@oultook.com>
2019-12-24 16:34:33 +0800

    build main chat scene

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    new MaterialApp(
-      title: "Friendlychat",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Friendlychat"),
-        ),
-      ),
-    ),
-  );
+void main() => runApp(WBChat());
+
+class WBChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "WB Chat",
+      home: new MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("WB Chat")),
+    );
+  }
 }
