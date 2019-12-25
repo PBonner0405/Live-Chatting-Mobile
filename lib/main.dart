97a6cbb68d7d931f642c556b691a7489383c265d
topdev10 <k100cool@oultook.com>
2019-12-25 12:10:39 +0800

    add firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() => runApp(WBChat());
 
